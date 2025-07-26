40ff26b7c0cdd98240e1bce6f6bec27fddf6b4fd
Ariful Islam <remalarif0130@gmail.com>
2025-07-27 00:27:20 +0600

    fav added

--- src/App.js
+++ src/App.js
@@ -8,29 +8,31 @@ import Alert from "./components/Alert";
 function App() {
   const [mode, setMode] = useState("light");
   const [text, setText] = useState("Dark Mode");
-  const[alert,setAlert] = useState(null)
+  const [alert, setAlert] = useState(null);
 
-  const showAlert =(message,type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type:type
-    })
-setTimeout(()=>{
-  setAlert(null)
-},2000)
-  }
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       setText("Light Mode");
       document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mode hasbeen enabeled","success")
+      showAlert("Dark mode hasbeen enabeled", "success");
+      document.title = "TextUtils-Dark Mode";
     } else {
       setMode("light");
       setText("Dark Mode");
 
       document.body.style.backgroundColor = "white";
-      showAlert("Light mode hasbeen enabeled","success")
+      showAlert("Light mode hasbeen enabeled", "success");
+      document.title = "TextUtils-Light Mode";
     }
   };
   return (
@@ -41,11 +43,14 @@ setTimeout(()=>{
         toggleMode={toggleMode}
         text={text}
       ></Navbar>
-        <Alert alert={alert}></Alert>
+      <Alert alert={alert}></Alert>
       <div className="container my-3">
-        <TextForm heading="Enter the text to Analyze Below" showAlert={showAlert}  mode={mode}></TextForm>
+        <TextForm
+          heading="Enter the text to Analyze Below"
+          showAlert={showAlert}
+          mode={mode}
+        ></TextForm>
         {/* <About></About> */}
-
       </div>
     </>
   );
